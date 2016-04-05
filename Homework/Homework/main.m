2809e2c5b6aaa1676f570603cc7ee0f13d1db999
ElAndrei <andrei.ivanov@elilink.com>
2016-04-05 16:21:04 +0300

    TEST

--- Homework/Homework/main.m
+++ Homework/Homework/main.m
@@ -1,16 +1,1 @@
-//
-//  main.m
-//  Homework
-//
-//  Created by Andrey Ivanov on 05/04/16.
-//  Copyright © 2016 Andrey Ivanov. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
-}
+1
