fd5325c8460aa80242c7cb3bd30fd64bd9334136
Andrey Ivanov <andrei.ivanov@elilink.com>
2016-04-05 14:51:22 +0300

    [DEV] add project

---
+++ Homework/Homework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework
+//
+//  Created by Andrey Ivanov on 05/04/16.
+//  Copyright © 2016 Andrey Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework/Homework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework
+//
+//  Created by Andrey Ivanov on 05/04/16.
+//  Copyright © 2016 Andrey Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
